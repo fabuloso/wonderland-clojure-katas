fe18140f66b2ed9e5cbd39580cc1435135b56761
fabuloso <dalfovo.alessandro@gmail.com>
2016-11-22 15:34:35 +0100

    First improve of alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,10 +1,55 @@
 (ns alphabet-cipher.coder)
 
+(defn char-int [letter]
+  (int letter)  
+  )
+
+(defn int-char [number]
+  (char number)
+  )
+
+(defn distance [letter]
+   (- 
+     (int letter) 
+     (int \a))
+   )
+
+(defn single-letter-work [first second]
+ (+ 
+   (- (int second) 97) 
+    (distance first))
+ ) 
+
+(defn cipher-single-char [letter base]
+  (char 
+    (+
+      (rem (single-letter-work letter base) 26) 
+    97))
+  ) 
+
+(defn fix-keyword [keyword length]
+  (take length (apply concat (repeat keyword)))
+  )
+
+(defn distance-bewtween [first second]
+  (- (int second) (int first)))
+   
+(defn distances [keyword message]
+  (map distance-bewtween message keyword))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str 
+         (map cipher-single-char 
+              (char-array message) 
+              (char-array 
+                (fix-keyword keyword 
+                             (count message )))))
+  )
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map char
+         (map #(+ 97 %) (distances keyword message))))
+  )
 
 (defn decipher [cipher message]
   "decypherme")
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-char-to-int
+  (testing "given a number i wanna its letter"
+    (is (= 97 (char-int \a))))
+  )
+
+(deftest test-int-to-char
+  (testing "given a char i want its number"
+  (is (= \a (int-char 97)))))
+
+(deftest distance-between-two-letters
+  (testing "i want to know the distance between two letters"
+    (is (= 1 (distance \b)))))
+
+(deftest cipher-a-with-b
+  (testing "i want to cipher an a with a b"
+    (is (= \b (cipher-single-char \a \b))))
+    (is (= \e (cipher-single-char \s \m)))
+    )
+
 (deftest test-encode
   (testing "can encode given a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
